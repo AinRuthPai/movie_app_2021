81bde45eefc90e7205f899a07ffcff4e9a22fffb
AinRuthPai <tanbing@naver.com>
2021-10-19 17:56:39 +0900

    state learning

--- src/App.js
+++ src/App.js
@@ -1,54 +1,39 @@
 import React from "react";
-import PropTypes from "prop-types";
 
-const foodILike = [
-  {
-    id: 1, 
-    name: "kimchi", 
-    num: 1,
-    rating: "5.0"
-  }, 
-  {
-    id: 2, 
-    name: "ramen", 
-    num: 2,
-    rating: "3.5"
-  },
-  {
-   id: 3, 
-   name: "kimbab", 
-   num: 3,
-   rating: "4.5"
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("constructor");
   }
-];
+  state = {
+    count: 0
+  };
 
-function Food({name, number, rating}) {
-  return <div>
-    <h2>I like {name}</h2>
-    <h3>{rating} / 5.0</h3>
-    <h3>this number is {number}</h3>
-  </div>
-}
+  plus = () => {
+    this.setState(current => ({count: current.count + 1}));
+  };
 
-Food.propTypes = {
-  name: PropTypes.string.isRequired,
-  number: PropTypes.number.isRequired,
-  rating: PropTypes.string.isRequired
+  minus = () => {
+    this.setState(current => ({count: current.count - 1}));
+  };
+
+  componentDidMount() {
+    console.log("component rendered");
+  }
+
+  componentDidUpdate() {
+    console.log("I just updated");
+  }
+
+  render() {
+    console.log("rendering");
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.plus}>Plus</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    )};
 };
-
-function App() {
-  return (
-    <div>
-    {foodILike.map(
-      dish => <Food 
-      key={dish.id} 
-      name={dish.name} 
-      number={dish.num}
-      rating={dish.rating}
-      />
-      )}
-    </div>
-  );
-}
 
 export default App;
